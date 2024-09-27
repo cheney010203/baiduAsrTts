3f88b2e536b6b54f262763a15cec0ff589da2900
徐佳 <Jia.Xu12@geely.com>
2024-09-27 17:00:39 +0800

    init

---
+++ uiasr/src/main/java/com/baidu/aip/asrwakeup3/uiasr/activity/ActivityUiRecog.java
@@ -0,0 +1,378 @@
+package com.baidu.aip.asrwakeup3.uiasr.activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import com.baidu.aip.asrwakeup3.core.recog.IStatus;
+import com.baidu.aip.asrwakeup3.core.util.AuthUtil;
+import com.baidu.aip.asrwakeup3.uiasr.params.AllRecogParams;
+import com.baidu.aip.asrwakeup3.uiasr.params.CommonRecogParams;
+import com.baidu.aip.asrwakeup3.uiasr.params.NluRecogParams;
+import com.baidu.aip.asrwakeup3.uiasr.params.OfflineRecogParams;
+import com.baidu.aip.asrwakeup3.uiasr.params.OnlineRecogParams;
+import com.baidu.aip.asrwakeup3.uiasr.setting.AllSetting;
+import com.baidu.aip.asrwakeup3.uiasr.setting.NluSetting;
+import com.baidu.aip.asrwakeup3.uiasr.setting.OfflineSetting;
+import com.baidu.aip.asrwakeup3.uiasr.setting.OnlineSetting;
+import com.baidu.aip.asrwakeup3.uiasr.tts.control.InitConfig;
+import com.baidu.aip.asrwakeup3.uiasr.tts.control.MySyntherizer;
+import com.baidu.aip.asrwakeup3.uiasr.tts.control.NonBlockSyntherizer;
+import com.baidu.aip.asrwakeup3.uiasr.tts.listener.UiMessageListener;
+import com.baidu.aip.asrwakeup3.uiasr.tts.util.Auth;
+import com.baidu.aip.asrwakeup3.uiasr.tts.util.AutoCheck;
+import com.baidu.aip.asrwakeup3.uiasr.tts.util.IOfflineResourceConst;
+import com.baidu.speech.asr.SpeechConstant;
+import com.baidu.tts.chainofresponsibility.logger.LoggerProxy;
+import com.baidu.tts.client.SpeechSynthesizer;
+import com.baidu.tts.client.SpeechSynthesizerListener;
+import com.baidu.tts.client.TtsMode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 识别的基类Activity。 ActivityCommon定义了通用的UI部分
+ * 封装了识别的大部分逻辑，包括MyRecognizer的初始化，资源释放
+ * <p>
+ * <p>
+ * 集成流程代码，只需要一句： myRecognizer.start(params);具体示例代码参见startRough()
+ * =》.实例化 myRecognizer   new MyRecognizer(this, listener);
+ * =》 实例化 listener  new MessageStatusRecogListener(null);
+ * </p>
+ * 集成文档： http://ai.baidu.com/docs#/ASR-Android-SDK/top 集成指南一节
+ * demo目录下doc_integration_DOCUMENT
+ * ASR-INTEGRATION-helloworld  ASR集成指南-集成到helloworld中 对应 ActivityMiniRecog
+ * ASR-INTEGRATION-TTS-DEMO ASR集成指南-集成到合成DEMO中 对应 ActivityUiRecog
+ * <p>
+ * 大致流程为
+ * 1. 实例化MyRecognizer ,调用release方法前不可以实例化第二个。参数中需要开发者自行填写语音识别事件的回调类，实现开发者自身的业务逻辑
+ * 2. 如果使用离线命令词功能，需要调用loadOfflineEngine。在线功能不需要。
+ * 3. 根据识别的参数文档，或者demo中测试出的参数，组成json格式的字符串。调用 start 方法
+ * 4. 在合适的时候，调用release释放资源。
+ * <p>
+ */
+
+public abstract class ActivityUiRecog extends ActivityCommon implements IStatus {
+
+    /*
+     * Api的参数类，仅仅用于生成调用START的json字符串，本身与SDK的调用无关
+     */
+    private final CommonRecogParams apiParams;
+
+    private final Class settingActivityClass;
+
+    /**
+     * 控制UI按钮的状态
+     */
+    protected int status;
+
+    /**
+     * 日志使用
+     */
+    private static final String TAG = "ActivityUiRecog";
+
+
+    /**
+     * 开始录音，点击“开始”按钮后调用。
+     */
+    protected abstract void start();
+
+
+    /**
+     * 开始录音后，手动停止录音。SDK会识别在此过程中的录音。点击“停止”按钮后调用。
+     */
+    protected abstract void stop();
+
+    /**
+     * 开始录音后，取消这次录音。SDK会取消本次识别，回到原始状态。点击“取消”按钮后调用。
+     */
+    protected abstract void cancel();
+
+    protected boolean running = false;
+
+
+    // ===============初始化参数设置完毕，更多合成参数请至getParams()方法中设置 =================
+
+    // 主控制类，所有合成控制方法从这个类开始
+    protected MySyntherizer synthesizer;
+
+    /**
+     * 发布时请替换成自己申请的appId appKey 和 secretKey。注意如果需要离线合成功能,请在您申请的应用中填写包名。
+     * 本demo的包名是com.baidu.tts.sample，定义在build.gradle中。
+     */
+    protected String appId;
+
+    protected String appKey;
+
+    protected String secretKey;
+    // TtsMode.MIX; 离在线融合，在线优先； TtsMode.ONLINE 纯在线； TtsMode.OFFLINE 纯离线合成，需要纯离线SDK
+    protected TtsMode ttsMode = IOfflineResourceConst.DEFAULT_SDK_TTS_MODE;
+
+    protected Handler mainHandler;
+    public ActivityUiRecog(int textId) {
+        super(textId);
+
+        String className = getClass().getSimpleName();
+        if (className.equals("ActivityOnlineRecog") || className.equals("ActivityUiDialog")) {
+            settingActivityClass = OnlineSetting.class;
+            apiParams = new OnlineRecogParams();
+        } else if (className.equals("ActivityOfflineRecog")) {
+            settingActivityClass = OfflineSetting.class;
+            apiParams = new OfflineRecogParams();
+        } else if (className.equals("ActivityNlu")) {
+            settingActivityClass = NluSetting.class;
+            apiParams = new NluRecogParams();
+        } else if (className.equals("ActivityAllRecog")) {
+            settingActivityClass = AllSetting.class;
+            apiParams = new AllRecogParams();
+        } else {
+            throw new RuntimeException("PLEASE DO NOT RENAME DEMO ACTIVITY, current name:" + className);
+        }
+
+
+    }
+
+    protected void handle(Message msg) {
+        int what = msg.what;
+    }
+
+    protected Map<String, Object> fetchParams() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        //  上面的获取是为了生成下面的Map， 自己集成时可以忽略
+        Map<String, Object> params = apiParams.fetch(sp);
+        params.put(SpeechConstant.APP_ID, AuthUtil.getAppId()); // 添加appId
+        params.put(SpeechConstant.APP_KEY, AuthUtil.getAk()); // 添加apiKey
+        params.put(SpeechConstant.SECRET, AuthUtil.getSk()); // 添加secretKey
+        //  集成时不需要上面的代码，只需要params参数。
+        return params;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        apiParams.initSamplePath(this);
+
+        try {
+            Auth.getInstance(this);
+        } catch (Auth.AuthCheckException e) {
+            Log.e("ActivityUiRecog","ActivityUiRecog,e:"+e.getLocalizedMessage());
+        }
+
+        appId = Auth.getInstance(this).getAppId();
+        appKey = Auth.getInstance(this).getAppKey();
+        secretKey = Auth.getInstance(this).getSecretKey();
+        initialTts(); // 初始化TTS引擎
+        mainHandler = new Handler() {
+            /*
+             * @param msg
+             */
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                handle(msg);
+            }
+
+        };
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        status = STATUS_NONE;
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                switch (status) {
+                    case STATUS_NONE: // 初始状态
+                        start();
+                        status = STATUS_WAITING_READY;
+                        updateBtnTextByStatus();
+                        txtLog.setText("");
+                        txtResult.setText("");
+                        break;
+                    case STATUS_WAITING_READY: // 调用本类的start方法后，即输入START事件后，等待引擎准备完毕。
+                    case STATUS_READY: // 引擎准备完毕。
+                    case STATUS_SPEAKING: // 用户开始讲话
+                    case STATUS_FINISHED: // 一句话识别语音结束
+                    case STATUS_RECOGNITION: // 识别中
+                        stop();
+                        status = STATUS_STOPPED; // 引擎识别中
+                        updateBtnTextByStatus();
+                        break;
+                    case STATUS_LONG_SPEECH_FINISHED: // 长语音识别结束
+                    case STATUS_STOPPED: // 引擎识别中
+                        cancel();
+                        status = STATUS_NONE; // 识别结束，回到初始状态
+                        updateBtnTextByStatus();
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        });
+        if (setting != null && settingActivityClass != null) {
+            setting.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    running = true; // 是否该Activity依旧需要运行
+                    Intent intent = new Intent(ActivityUiRecog.this, settingActivityClass);
+                    startActivityForResult(intent, 1);
+                }
+            });
+
+        }
+    }
+
+    //TODO:xujia
+
+    private void speak(String ttsContent) {
+
+        List texts = new ArrayList<>();
+        if(ttsContent.length()>20){
+            int count=ttsContent.length()/20+1;
+            for (int i = 0; i < count; i++) {
+                if(i==(count-1)){
+                    texts.add(new Pair(ttsContent.substring(i*20,ttsContent.length()),(i+1)+""));
+                }else{
+                    texts.add(new Pair(ttsContent.substring(i*20,(i+1)*20),(i+1)+""));
+                }
+            }
+            int result = synthesizer.batchSpeak(texts);
+        }else{
+            int result = synthesizer.speak(ttsContent);
+        }
+
+
+//
+//        int result = synthesizer.speak(text);
+//        checkResult(result, "speak");
+    }
+
+
+    /**
+     * 合成的参数，可以初始化时填写，也可以在合成前设置。
+     *
+     * @return 合成参数Map
+     */
+    protected Map<String, String> getParams() {
+        Map<String, String> params = new HashMap<>();
+        // 以下参数均为选填
+        // 设置在线发声音人： 0 普通女声（默认） 1 普通男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>, 其它发音人见文档
+        params.put(SpeechSynthesizer.PARAM_SPEAKER, "0");
+        // 设置合成的音量，0-15 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_VOLUME, "15");
+        // 设置合成的语速，0-15 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_SPEED, "5");
+        // 设置合成的语调，0-15 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_PITCH, "5");
+        return params;
+    }
+    protected InitConfig getInitConfig(SpeechSynthesizerListener listener) {
+        Map<String, String> params = getParams();
+        // 添加你自己的参数
+        InitConfig initConfig;
+        // appId appKey secretKey 网站上您申请的应用获取。注意使用离线合成功能的话，需要应用中填写您app的包名。包名在build.gradle中获取。
+
+        initConfig = new InitConfig(appId, appKey, secretKey, ttsMode, params, listener);
+
+        // 如果您集成中出错，请将下面一段代码放在和demo中相同的位置，并复制InitConfig 和 AutoCheck到您的项目中
+        // 上线时请删除AutoCheck的调用
+        AutoCheck.getInstance(getApplicationContext()).check(initConfig, new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 100) {
+                    AutoCheck autoCheck = (AutoCheck) msg.obj;
+                    synchronized (autoCheck) {
+                        String message = autoCheck.obtainDebugMessage();
+//                        toPrint(message); // 可以用下面一行替代，在logcat中查看代码
+                        // Log.w("AutoCheckMessage", message);
+                    }
+                }
+            }
+
+        });
+        return initConfig;
+    }
+
+
+    /**
+     * 初始化引擎，需要的参数均在InitConfig类里
+     * <p>
+     * DEMO中提供了3个SpeechSynthesizerListener的实现
+     * MessageListener 仅仅用log.i记录日志，在logcat中可以看见
+     * UiMessageListener 在MessageListener的基础上，对handler发送消息，实现UI的文字更新
+     * FileSaveListener 在UiMessageListener的基础上，使用 onSynthesizeDataArrived回调，获取音频流
+     */
+    protected void initialTts() {
+        LoggerProxy.printable(true); // 日志打印在logcat中
+        // 设置初始化参数
+        // 此处可以改为 含有您业务逻辑的SpeechSynthesizerListener的实现类
+        SpeechSynthesizerListener listener = new UiMessageListener(mainHandler);
+        InitConfig config = getInitConfig(listener);
+        synthesizer = new NonBlockSyntherizer(this, config, mainHandler); // 此处可以改为MySyntherizer 了解调用过程
+    }
+
+    protected void handleMsg(Message msg) {
+        super.handleMsg(msg);
+
+        switch (msg.what) { // 处理MessageStatusRecogListener中的状态回调
+            case STATUS_FINISHED:
+                if (msg.arg2 == 1) {
+                    txtResult.setText(msg.obj.toString());
+                    //TODO: xujia  tts
+                    speak(msg.obj.toString());
+                }
+                status = msg.what;
+                updateBtnTextByStatus();
+                break;
+            case STATUS_NONE:
+            case STATUS_READY:
+            case STATUS_SPEAKING:
+            case STATUS_RECOGNITION:
+                status = msg.what;
+                updateBtnTextByStatus();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    private void updateBtnTextByStatus() {
+        switch (status) {
+            case STATUS_NONE:
+                btn.setText("开始录音");
+                btn.setEnabled(true);
+                setting.setEnabled(true);
+                break;
+            case STATUS_WAITING_READY:
+            case STATUS_READY:
+            case STATUS_SPEAKING:
+            case STATUS_RECOGNITION:
+                btn.setText("停止录音");
+                btn.setEnabled(true);
+                setting.setEnabled(false);
+                break;
+            case STATUS_LONG_SPEECH_FINISHED:
+            case STATUS_STOPPED:
+                btn.setText("取消整个识别过程");
+                btn.setEnabled(true);
+                setting.setEnabled(false);
+                break;
+            default:
+                break;
+        }
+    }
+}
